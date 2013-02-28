ef43e6fa4c07f44740c99a0a3ab0f49afcfe95ab
Ryan Feng <odayfans@gmail.com>
2013-02-28 08:19:25 -0500

    Fix ftest

--- tests/ftest.hs
+++ tests/ftest.hs
@@ -4,34 +4,42 @@ import IRC.FMessage as M
 
 import System.Environment
 import System.IO
-import Data.Attoparsec.Combinator
-import Data.Attoparsec.ByteString.Char8 (IResult(..), parse)
-import qualified Data.ByteString.Char8 as B
+import Data.Attoparsec.Text (IResult(..), parse)
+import qualified Data.Text as T
+import qualified Data.Text.IO as TO
 import Control.Monad
 import System.Exit
 
 parseLine :: Handle -> IO ()
-parseLine inh = B.hGetLine inh >>= \x ->
-  case (parse (many' message) . (`B.append` "\n")) x of
-    Done r res -> if (not . B.null) r
-                   then putStrLn $ "Parse error, remaining" ++ B.unpack r
-                   else print $ M.command $ head res
+parseLine inh = TO.hGetLine inh >>= \x ->
+  case parse message x of
+    Done r res -> if (not . T.null) r
+                   then putStrLn $ "Parse error, remaining" ++ T.unpack r
+                   else TO.putStrLn $ M.rawShow $ M.command res
     Fail _ _ e -> putStrLn $ "Parse error: " ++ e
     _ -> putStrLn "Need more input"
 
 parseLineToFile :: Handle -> Handle -> IO ()
-parseLineToFile inh outh = B.hGetLine inh >>= hPrint outh . parse (many' message) . (`B.append` "\n")
+parseLineToFile inh outh =
+  liftM (parse message . (`T.append` "\r\n")) (TO.hGetLine inh) >>=
+    \x -> case x of
+      Done _ r -> TO.hPutStrLn outh $ rawShow r
+      Fail _ _ e -> putStrLn ("Parse error: " ++ e) >> exitFailure
+      _ -> putStrLn "Need more input" >> exitFailure
 
 main ::  IO ()
 main = do
   args <- getArgs
   if length args < 2
-  then putStrLn "Need <input file> and <output file>" >> exitSuccess
-  else
-    let inf = head args
-        outf = head $ tail args
-    in
-      withFile inf ReadMode $ \h ->
-        if head outf == '-'
-        then forever $ parseLine h
-        else withFile outf WriteMode $ \hh -> forever $ parseLineToFile h hh
+    then putStrLn "Need <input file> and <output file>" >> exitSuccess
+    else
+      let inf = head args
+          outf = head $ tail args
+      in
+        withFile inf ReadMode $ \h ->
+          hSetNewlineMode h (NewlineMode CRLF CRLF) >>
+          if head outf == '-'
+          then forever $ parseLine h
+          else withFile outf WriteMode $ \hh -> do
+            hSetNewlineMode hh (NewlineMode CRLF CRLF)
+            forever $ parseLineToFile h hh
