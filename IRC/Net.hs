28cbb337bbf15c6fa43c8fd94594072f91405ad5
Ryan Feng <odayfans@gmail.com>
2013-03-15 03:09:31 -0400

    Add disconnecServ

--- IRC/Net.hs
+++ IRC/Net.hs
@@ -2,13 +2,23 @@
 module IRC.Net
 (
   connectToServ
+, disconnectServ
 )
 where
 
 import Network
 import System.IO
+import System.Time
+
+import IRC.Tunnel
+import IRC.Internal
+import IRC.Util
 
 connectToServ :: HostName -> PortNumber -> IO Tunnel
 connectToServ host port = withSocketsDo $ do
   h <- connectTo host (PortNumber port) >>= setupHandle
-  return $ newTunnel (Server host port h) 1024
+  time <- getClockTime
+  newTunnel (Server host port h time) 1024
+
+disconnectServ :: Server -> IO ()
+disconnectServ = hClose . sHandle
