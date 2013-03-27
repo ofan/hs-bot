91f33077aa43b899427bb92ab935ce39e363809d
Ryan Feng <odayfans@gmail.com>
2013-03-27 03:53:03 -0400

    Replace lift with liftIO for IO operations in proxies

--- IRC/Proxy.hs
+++ IRC/Proxy.hs
@@ -14,6 +14,7 @@ import Control.Proxy
 
 import IRC.Internal
 import IRC.Tunnel
+import Control.Concurrent.STM.TQueue
 
 -- | @chanS up down@ is a channel server that will write values from downstream to @up@ channel,
 -- and write values from @down@ to downstream.
@@ -21,11 +22,11 @@ chanS :: ReadChan -> WriteChan -> PluginS
 chanS up down = foreverK go
   where go m = case m of
           Nothing -> do -- Downstream requets for new message
-            inmsg <- lift $ readChan down
+            inmsg <- liftIO $ readChan down
             m' <- respond $ Just inmsg
             go m'
           Just m'' -> do
-            lift $ writeChan up m''
+            liftIO $ writeChan up m''
             n <- respond Nothing
             go n
 
@@ -33,11 +34,11 @@ chanS up down = foreverK go
 -- and write values from @up@ to upstream.
 chanC :: ReadChan -> WriteChan -> PluginC
 chanC up down () = forever $ do
-  inmsg <- lift $ readChan up
+  inmsg <- liftIO $ readChan up
   m <- request $ Just inmsg
   case m of
     Nothing -> return ()
-    Just m' -> lift $ writeChan down m'
+    Just m' -> liftIO $ writeChan down m'
 
 -- | @chanU@ writes all values flowing upstream to a channel.
 chanU :: ReadChan -> PluginU
@@ -45,7 +46,7 @@ chanU up = foreverK go
   where go m = do
           case m of
             Nothing -> request m
-            Just m' -> lift $ writeChan up m'
+            Just m' -> liftIO $ writeChan up m'
           n <- respond ()
           go n
 
@@ -55,7 +56,7 @@ chanD down () = forever $ do
   m <- request ()
   case m of
     Nothing -> respond Nothing
-    Just m' -> lift $ writeChan down m'
+    Just m' -> liftIO $ writeChan down m'
 
 -- | A wrapper around 'Tunnel' for Proxy Server, it will return Nothing if the tunnel is empty,
 -- that is, it won't block.
@@ -63,13 +64,13 @@ tunnelS :: Tunnel -> PluginS
 tunnelS t = foreverK go
   where go m = case m of
           Nothing -> do -- Client requests for new message
-            inmsg <- lift $ tryReadTunnel t
+            inmsg <- liftIO $ tryReadTunnel t
             m'' <- respond inmsg
             go m''
           -- | Write the message to the channel, then send Nothing to downstream, the downstream should
           -- discard received Nothing at all times.
           Just m' -> do
-            lift $ writeTunnel t m'
+            liftIO $ writeTunnel t m'
             n <- respond Nothing
             go n
 
